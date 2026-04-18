e55e55f1da14b1c12f946edb7d8b4be0da4720e3
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2026-04-18 18:40:03 +0530

    chore: clean upsert code and api

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from services.sentence_transformer import convert_text_to_embeddings_service
+from services.neo4j import upsert_to_neo4j_service
 
 app = FastAPI()
+
+class IUpsertToNeo4j(BaseModel):
+    text: str
 
 @app.get("/")
 def root():
@@ -17,3 +22,11 @@ def convert_text_to_embeddings(sentences: list[str]):
         if isinstance(e, RuntimeError):
             return {"error": str(e)}
         return {"error": "An unexpected error occurred in python service."}
+    
+@app.post("/neo4j/upsert")
+def upsert_text_to_neo4j(payload: IUpsertToNeo4j):
+    try:
+        result = upsert_to_neo4j_service(payload.text)
+        return {"success": True, "result": result}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
